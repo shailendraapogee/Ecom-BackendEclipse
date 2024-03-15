b37489ff8ac3ae52248d506a014625cfc43ec1b0
shailu08 <shailendrasinghapogee@gmail.com>
2024-03-15 15:08:28 +0530

    change in login controller.

--- src/main/java/com/apogee/controller/LoginController.java
+++ src/main/java/com/apogee/controller/LoginController.java
@@ -51,90 +51,90 @@ public class LoginController {
 //		return "/WEB-INF/jsp/order";
 	}
 
-//	@PostMapping("/login")
-////    public String authenticateUser(@RequestBody LoginRequest loginRequest, Model model) {
-//	public String authenticateUser(@RequestParam String username, @RequestParam String password, Model model,
-//			HttpSession session) {
-//		User user = userService.validateUser(username, password);
-//		if (user != null) {
-//			session.setAttribute("email", user.getEmail());
-//			session.setAttribute("username", user.getName());
-//			session.setAttribute("role", "Admin");
-//
-//			int sessionTimeoutInSeconds = 300; // 5 minutes
-//			session.setMaxInactiveInterval(sessionTimeoutInSeconds);
-//
-//			return "redirect:/dashboard";
-//		} else {
-//			model.addAttribute("error", "Invalid username or password");
-//			return "loginpage";
-//		}
-//	}
-//
-//	@GetMapping("/user")
-//	public String showUserPage(HttpSession session) {
-//		if (session.getAttribute("username") == null) {
-//			return "redirect:/loginpage";
-//		} else {
-//			return "user";
-//		}
-//	}
-//
-//	@GetMapping("/product")
-//	public String showProductPage(HttpSession session) {
-//		if (session.getAttribute("username") == null) {
-//			return "redirect:/loginpage";
-//		} else {
-//			return "product";
-//		}
-//	}
-//
-//	@GetMapping("/order")
-//	public String showOrderPage(HttpSession session) {
-//		if (session.getAttribute("username") == null) {
-//			return "redirect:/loginpage";
-//		} else {
-//			return "order";
-//		}
-//
-//	}
-//
-//	@GetMapping("/header")
-//	public String HeaderTesting() {
-//		return "header";
-//	}
-//
-//	@GetMapping("/shopingcart")
-//	public String Shopingcart(HttpSession session) {
-//		if (session.getAttribute("username") == null) {
-//			return "redirect:/loginpage";
-//		} else {
-//			return "shopingcart";
-//		}
-//	}
-//
-//	@GetMapping("/chart")
-//	public String Chart() {
-//		return "chart";
-//	}
-//
-//	@GetMapping("/about")
-//	public String About() {
-//		return "about";
-//	}
-//
-//	@GetMapping("/signup")
-//	public String Signup() {
-//		return "signup";
-//	}
-//
-//	@GetMapping("/category")
-//	public String Category(HttpSession session) {
-//		if (session.getAttribute("username") == null) {
-//			return "redirect:/loginpage";
-//		} else {
-//			return "category";
-//		}
-//	}
+	@PostMapping("/login")
+//    public String authenticateUser(@RequestBody LoginRequest loginRequest, Model model) {
+	public String authenticateUser(@RequestParam String username, @RequestParam String password, Model model,
+			HttpSession session) {
+		User user = userService.validateUser(username, password);
+		if (user != null) {
+			session.setAttribute("email", user.getEmail());
+			session.setAttribute("username", user.getName());
+			session.setAttribute("role", "Admin");
+
+			int sessionTimeoutInSeconds = 300; // 5 minutes
+			session.setMaxInactiveInterval(sessionTimeoutInSeconds);
+
+			return "redirect:/dashboard";
+		} else {
+			model.addAttribute("error", "Invalid username or password");
+			return "WEB-INF/view/loginpage";
+		}
+	}
+
+	@GetMapping("/user")
+	public String showUserPage(HttpSession session) {
+		if (session.getAttribute("username") == null) {
+			return "redirect:/loginpage";
+		} else {
+			return "WEB-INF/view/user";
+		}
+	}
+
+	@GetMapping("/product")
+	public String showProductPage(HttpSession session) {
+		if (session.getAttribute("username") == null) {
+			return "redirect:/loginpage";
+		} else {
+			return "WEB-INF/view/product";
+		}
+	}
+
+	@GetMapping("/order")
+	public String showOrderPage(HttpSession session) {
+		if (session.getAttribute("username") == null) {
+			return "redirect:/loginpage";
+		} else {
+			return "WEB-INF/view/order";
+		}
+
+	}
+
+	@GetMapping("/header")
+	public String HeaderTesting() {
+		return "WEB-INF/view/header";
+	}
+
+	@GetMapping("/shopingcart")
+	public String Shopingcart(HttpSession session) {
+		if (session.getAttribute("username") == null) {
+			return "redirect:/loginpage";
+		} else {
+			return "WEB-INF/view/shopingcart";
+		}
+	}
+
+	@GetMapping("/chart")
+	public String Chart() {
+		return "WEB-INF/view/chart";
+	}
+
+	@GetMapping("/about")
+	public String About() {
+		return "WEB-INF/view/about";
+	}
+
+	@GetMapping("/signup")
+	public String Signup() {
+		return "WEB-INF/view/signup";
+	}
+
+	@GetMapping("/category")
+	public String Category(HttpSession session) {
+		if (session.getAttribute("username") == null) {
+			return "redirect:/loginpage";
+		} else {
+			return "WEB-INF/view/category";
+		}
+	}
 
 }
